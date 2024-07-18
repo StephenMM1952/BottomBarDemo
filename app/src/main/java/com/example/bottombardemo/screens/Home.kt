2e0c7782a72143c6b72a607830b99fc2960dd838
Stephen Muchow <smm@osbornelodge.com>
2024-07-18 17:11:23 +0100

    Changed Home buttons to size(52.dp)

--- app/src/main/java/com/example/bottombardemo/screens/Home.kt
+++ app/src/main/java/com/example/bottombardemo/screens/Home.kt
@@ -3,10 +3,12 @@ package com.example.bottombardemo.screens
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -25,13 +27,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -89,25 +94,36 @@ fun Home(viewModel: HomeViewModel = viewModel()) {
                     horizontalArrangement = Arrangement.SpaceBetween
                 )
                 {
-                    for (c in 0..4) {
+                    // TODO: Add Delete Button Here//
+
+                    for (c in 0..4)
+                    {
                         Button(
                             onClick = { viewModel.nextColor(it, c) },
                             modifier = Modifier
                                 .clip(CircleShape) // Make the button circular
-                                .size(56.dp),
+                                .size(52.dp),
                             colors = ButtonDefaults.buttonColors(
                                 containerColor = clueColorMap.getValue(clueStatus[c].toString()),
                                 contentColor = White
                             )
                         )
                         {
-                            Text(
-                                text = clueWord[c].toString(),
-                                fontSize = 20.sp
-                            )
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Text(
+                                    text = clueWord[c].toString(),
+                                    textAlign = TextAlign.Center,
+                                    fontSize = 30.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    fontFamily = FontFamily.Monospace,
+                                )
+                            }
                         }
-
                     }
+                    // TODO: Add Bin Button Here//
                 }
             }
         }
